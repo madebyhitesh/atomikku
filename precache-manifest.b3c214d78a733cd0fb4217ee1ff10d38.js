3957f708263998895ee62984cc7ac196f240d221
hitesh-webRepo <webdev.hitesh@gmail.com>
2022-06-05 21:06:19 +0530

    Updates

---
+++ precache-manifest.b3c214d78a733cd0fb4217ee1ff10d38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260e41c49a612f570636dbd3a7395b67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3bf2b4a1fa170d4e660b",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "dccce87055129de4cab9",
+    "url": "./static/js/2.f650a45d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f650a45d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf2b4a1fa170d4e660b",
+    "url": "./static/js/main.cdda8b7c.chunk.js"
+  },
+  {
+    "revision": "88b0d9a2beec3645840f",
+    "url": "./static/js/runtime-main.ed0da746.js"
+  }
+]);
